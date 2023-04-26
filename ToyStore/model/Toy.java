7f4c66455ecd122316622eb26ad9632b10db3644
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-04-26 18:17:08 +0300

    class Toy is updated

--- ToyStore/model/Toy.java
+++ ToyStore/model/Toy.java
@@ -14,6 +14,45 @@ public class Toy {
                 " weight " + this.getWeight();
     }
 
+    public Toy() {
+    }
 
-    
+    public Toy(int id, String name, int quantity, int weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
