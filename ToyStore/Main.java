39777bbe91a982c05f594bbf3119e17f9dd26541
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-04-26 18:06:55 +0300

    Main is added

---
+++ ToyStore/Main.java
@@ -0,0 +1,14 @@
+import global.MyFileWriter;
+
+import model.Toy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static global.Vars.allToys;
+import static global.Vars.winsToys;
+
+public class Main {
+    
+}
