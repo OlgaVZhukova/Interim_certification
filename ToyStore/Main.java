ffd29ce73ad69be416ccab3bd3a9826b1441847e
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-04-26 18:08:10 +0300

    Method of creation of variable toys is added

--- ToyStore/Main.java
+++ ToyStore/Main.java
@@ -10,5 +10,15 @@ import static global.Vars.allToys;
 import static global.Vars.winsToys;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        //Сначала создаем произвольные игрушки
+        Toy cat = new Toy(1, "cat", 10, 20);
+        Toy dog = new Toy(2, "dog", 3, 5);
+        Toy bird = new Toy(3, "bird", 4, 10);
+        Toy fish = new Toy(4, "fish", 10, 30);
+        Toy spiderman = new Toy(5, "spiderman", 6, 20);
+        Toy superman = new Toy(6, "superman", 5, 8);
+
+
+        
 }
